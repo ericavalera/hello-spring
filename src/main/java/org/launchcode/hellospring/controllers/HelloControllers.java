c47e7fc4aeb6431361f009714cda651beda6f5bb
Erica V <132383093+ericavalera@users.noreply.github.com>
2024-09-29 19:30:58 -0400

    added controllers

--- src/main/java/org/launchcode/hellospring/controllers/HelloControllers.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloControllers.java
@@ -2,6 +2,7 @@ package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -9,16 +10,21 @@ public class HelloControllers {
 
 //    handles requests at path /hello
 
-    @GetMapping("hello")
-    @ResponseBody
-    public String hello(){
-        return "Hello, Spring!";
-    }
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello(){
+//        return "Hello, Spring!";
+//    }
 
     @GetMapping("goodbye")
     @ResponseBody
     public String goodBye(){
         return "Goodbye, Spring!";
     }
-
+// Handles requests of the form /hello?name=LaunchCode
+    @GetMapping("hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name){
+        return "Hello, " + name + "!";
+    }
 }
