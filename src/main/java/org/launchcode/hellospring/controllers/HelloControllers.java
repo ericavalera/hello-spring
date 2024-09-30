a2235860738341cdfc8fce71523816241a3e26db
Erica V <132383093+ericavalera@users.noreply.github.com>
2024-09-30 15:36:07 -0400

    added form and language options

--- src/main/java/org/launchcode/hellospring/controllers/HelloControllers.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloControllers.java
@@ -3,47 +3,57 @@ package org.launchcode.hellospring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+
 @Controller
 @ResponseBody
-@RequestMapping("hello")
 public class HelloControllers {
-    //handle request at path http://localhost:8080/hello
 
-//    @GetMapping("hello")
-//    @ResponseBody
-//    public String hello(){
-//        return "Hello, Spring!";
-//    }
 
-    //handle request at path //http://localhost:8080/hello/goodbye
-    @GetMapping("goodbye")
-    public String goodbye(){
-        return "Goodbye, Spring!";
+//    form request
+    @RequestMapping(value="hello", method= RequestMethod.POST)
+    @ResponseBody
+    public String helloPost(@RequestParam String name, @RequestParam String lang){
+        if(name == null){
+            name = "World";
+        }
+        return createMessage(name, lang);
     }
 
-    //handle requests of the form http://localhost:8080/hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name){
-        return "Hello, " + name + "!";
+//    static method
+    public static String createMessage(String name, String lang){
+    String greeting ="";
+    if(lang.equals("english")){
+        greeting = "Hello";
+    } else if (lang.equals("french")){
+        greeting = "Bonjour";
+    } else if (lang.equals("spanish")){
+        greeting = "Hola";
+    } else if (lang.equals("german")){
+        greeting = "Guten tag";
+    } else if (lang.equals("italian")){
+        greeting = "Ciao";
     }
+    return greeting + " " + name;
+    };
 
-    //handle requests of the form http://localhost:8080/hello/LaunchCode
-    @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name){
-        return "Hello, " + name + "!";
-    }
 
     //handles requests for http://localhost:8080/hello/form
     @GetMapping("form")
     public String helloForm() {
         return "<html>" +
                 "<body>" +
-                "<form action = '/hello' method = 'post'>" + // submit a request to /hello
+                "<form action = '/hello' method ='post'>" + // submit a request to /hello
                 "<input type = 'text' name = 'name' >" +
                 "<select name='lang'>" +
                 "<option value= 'french'> french" +
                 "</option>" +
                 "<option value= 'spanish'> spanish" +
+                "</option>" +
+                "<option value= 'german'> german" +
+                "</option>" +
+                "<option value= 'italian'> italian" +
+                "</option>" +
+                "<option value= 'english'> english" +
                 "</option>" +
                 "</select>" +
                 "<input type = 'submit' value = 'Greet Me!' >" +
