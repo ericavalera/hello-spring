05385ad2d27b94577dd4c4c621a6208c702de35d
Erica V <132383093+ericavalera@users.noreply.github.com>
2024-10-07 18:22:40 -0400

    added form and language options

--- src/main/java/org/launchcode/hellospring/controllers/HelloControllers.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloControllers.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class HelloControllers {
     //handle request at path http://localhost:8080/hello
@@ -42,4 +45,13 @@ public class HelloControllers {
         return "form";
     }
 
+    @GetMapping("hello-names")
+public String helloNames(Model model){
+        List<String> names = new ArrayList<>();
+        names.add("LaunchCode");
+        names.add("Java");
+        names.add("JavaScript");
+        model.addAttribute("names", names);
+        return "hello-list";
+    }
 }
